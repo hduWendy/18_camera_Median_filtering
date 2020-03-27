ebc211131197c0046ecaa2c22eb8fc49c06176e0
hduWendy <576939544@qq.com>
2020-03-27 14:17:05 +0800

    ok

--- 18_camera_Median_filtering/18_camera_Median_filtering.cpp
+++ 18_camera_Median_filtering/18_camera_Median_filtering.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    return 0;
+	VideoCapture cap(0);
+	double scale = 0.5;
+
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat hsvMat;
+		cv::Mat medianMat;
+
+		cap >> frame;
+		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+
+		medianBlur(rFrame, medianMat, 5);
+
+		cv::imshow("whie: in the Median filtering", medianMat);
+		cv::imshow("frame", rFrame);
+
+		waitKey(30);
+	}
 }
 
